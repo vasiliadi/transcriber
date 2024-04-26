5e19447d74f5ccfe35219266e9f06dd3558f7755
vasiliadi <9432426+vasiliadi@users.noreply.github.com>
2024-04-26 00:55:33 -0500

    Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,114 @@
-# transcriber
-Transcriber &amp; translator for audio files. Like Otter.ai but free and build with Gemini 1.5 Pro.
+# The Transcriber
+Transcriber &amp; translator for audio files. Like Otter.ai but open-source and almost free.
+
+![Screenshot](screenshot.png)
+
+## Otter.ai
+
+[Otter.ai](https://otter.ai/pricing) monthly subscription is **$16.99**/per user. \
+Where you get:
+> 1200 monthly transcription minutes; 90 minutes per conversation
+
+## The Transcriber:
+
+**Transcription**:
+[Replicate AI models cloud-hosting](https://replicate.com/pricing) with current prices and models used, 1200 minutes will cost approximately **$5.50** \
+At least three times cheaper with the same or even better quality of transcription, in my opinion. \
+And you pay as you go.
+
+**Translation and summerization**:
+[Gemini 1.5 Pro](https://ai.google.dev/pricing) till May 2, 2024 is free. Then TBA.
+
+**Hosting**:
+Free tires of [Google Cloud](https://cloud.google.com/free), [Orcale Cloud](https://www.oracle.com/cloud/free/), [AWS](https://aws.amazon.com/free/), [Render](https://render.com/pricing), [Azure](https://azure.microsoft.com/en-us/pricing/free-services), [IBM Cloud](https://www.ibm.com/cloud/free), [DigitalOcean](https://www.digitalocean.com/) or any you like.
+
+## Technical details
+
+[Run Whisper model on Replicate](https://replicate.com/openai/whisper) much cheaper than using [OpenAI API for Whisper](https://openai.com/pricing).
+
+I use two models:
+
+[vaibhavs10/incredibly-fast-whisper](https://replicate.com/vaibhavs10/incredibly-fast-whisper) best for speed \
+[thomasmol/whisper-diarization](https://replicate.com/thomasmol/whisper-diarization) best for dialogs
+
+Same audio 45 minutes (6 speakers) comparison by model
+![Comparison of processing times by model](model-comparison.png)
+
+### Limitations
+
+#### OpenAI Whisper model
+
+[OpenAI Speech to text Whisper model](https://platform.openai.com/docs/guides/speech-to-text)
+
+> File uploads are currently limited to 25 MB.
+
+To avoid this limitation, I use compression. The file size without compression is 63 MB for 45 minutes of audio. However, after compression, the file size reduces to 4 MB for the same duration. Therefore, using compression, we can avoid splitting audio into chunks, and we can increase the limit to approximately 3 hours and 45 minutes of audio without losing transcription quality.
+
+But if you still need to transcript more you can split file using [pydub's](https://github.com/jiaaro/pydub/blob/master/API.markdown) `silence.split_on_silence()` or `silence.detect_silence()` or `silence.detect_nonsilent()`. This function's speed is hardware-dependent, but it is about 10 times faster than listening to the entire file.
+
+In my tests, I face three main problems:
+1. These functions are not working as I expect.
+2. If split just by time, you can cut in the middle of a word.
+3. Post-processing becomes a challenge. It's hard to identify the speaker smoothly. Loss of timestamps.
+
+All this beloongs to very long audio only.
+
+#### Gemini 1.5 Pro
+
+[Gemini 1.5 Pro model name and properties](https://cloud.google.com/vertex-ai/generative-ai/docs/learn/models)
+
+> Max output tokens: 8,192 
+
+0.75 words per token = ~6,144 words or about 35 minutes of speaking. But for [non-English languages](https://mor10.com/openai-token-tax/), most words are counted as two or more tokens.
+
+The maximum number of tokens for output is currently 8,192. Audio post-processing, which includes correction and translation, can only be done for files that are approximately 35 minutes long. Other models have a maximum output of 4,096 or less. If you need to process more than 8,192 tokens, you may need to do it in batches, but this will significantly increase the processing time.
+
+Translation by chunks still works, but the quality little bit lower.
+
+> Max audio length: approximately 8.4 hours
+
+It still works well for summarization.
+
+> [2 queries per minute, 1000 queries per day](https://ai.google.dev/gemini-api/docs/models/gemini#model-variations)
+
+[Languages support](https://cloud.google.com/vertex-ai/generative-ai/docs/learn/models#language-support) for translation.
+
+
+### Docs
+
+#### Config secrets
+
+Example of `.env` file:
+```
+GEMINI_API_KEY = "your_api_key"
+REPLICATE_API_TOKEN = "your_api_key"
+```
+
+You need to replace the path to env_file in `compose.yaml`
+
+[Get Gemini API key](https://ai.google.dev/) \
+[Get Replicate API key](https://replicate.com/account/api-tokens)
+
+
+#### Libraries
+
+[pytube](https://pytube.io/en/latest/)
+
+#### Streamlit
+
+[Develop locally with secrets](https://docs.streamlit.io/deploy/streamlit-community-cloud/deploy-your-app/secrets-management#develop-locally-with-secrets) \
+[Secrets management](https://docs.streamlit.io/develop/concepts/connections/secrets-management)
+
+#### Docker
+
+[Docker Best Practices](https://testdriven.io/blog/docker-best-practices/)
+
+[Docker](https://docs.docker.com/language/python/) \
+[Dockerfile reference](https://docs.docker.com/reference/dockerfile/) \
+[Dockerfile Linter](https://hadolint.github.io/hadolint/) \
+[.dockerignore](https://docs.docker.com/build/building/context/#dockerignore-files)
+
+[Docker Compose](https://docs.docker.com/compose/) \
+[Syntax for environment files in Docker Compose](https://docs.docker.com/compose/environment-variables/env-file/) \
+[Ways to set environment variables with Compose](https://docs.docker.com/compose/environment-variables/set-environment-variables/) \
+[Compose file version 3 reference](https://docs.docker.com/compose/compose-file/compose-file-v3/)
---
+++ streamlit_app.py
@@ -0,0 +1,244 @@
+import streamlit as st
+import os
+import subprocess
+import time
+import json
+import google.generativeai as genai
+import replicate
+import requests
+from pytube import YouTube
+
+# Google Gemini config
+gemini_api_key = os.environ["GEMINI_API_KEY"]
+genai.configure(api_key=gemini_api_key)
+generation_config = {
+    #   "temperature": 1,
+    #   "top_p": 0.95,
+    #   "top_k": 0,
+    "max_output_tokens": 8192,
+}
+gemini_safety_settings = [
+    {"category": "HARM_CATEGORY_HARASSMENT", "threshold": "BLOCK_NONE"},
+    {"category": "HARM_CATEGORY_HATE_SPEECH", "threshold": "BLOCK_NONE"},
+    {"category": "HARM_CATEGORY_SEXUALLY_EXPLICIT", "threshold": "BLOCK_NONE"},
+    {"category": "HARM_CATEGORY_DANGEROUS_CONTENT", "threshold": "BLOCK_NONE"},
+]
+model = genai.GenerativeModel(
+    "models/gemini-1.5-pro-latest",
+    generation_config=generation_config,
+    safety_settings=gemini_safety_settings,
+)
+
+# Replicate.com config
+replicate_api_token = os.environ["REPLICATE_API_TOKEN"]
+replicate_client = replicate.Client(api_token=replicate_api_token)
+
+# File names
+audio_file_name = "audio.mp3"
+converted_file_name = "audio.ogg"
+
+# Initialization
+if "mode" not in st.session_state:
+    st.session_state.mode = "Uploaded file"
+    st.session_state.language = None
+    st.session_state.model_name = "Balanced"
+
+
+# Functions
+def download(mode=st.session_state.mode):
+    match mode:
+        case "Uploaded file":
+            with open(audio_file_name, "wb") as f:
+                f.write(uploaded_file.getbuffer())
+        case "YouTube link":
+            YouTube(yt_url).streams.filter(only_audio=True).order_by(
+                "abr"
+            ).asc().first().download(filename=audio_file_name)
+        case "Audio file link":
+            downloaded_file = requests.get(audio_link)
+            with open(audio_file_name, "wb") as f:
+                f.write(downloaded_file.content)
+
+
+def compress_audio(audio_file_name=audio_file_name):
+    try:
+        convert_process = subprocess.run(
+            [
+                "ffmpeg",
+                "-y",
+                "-i",
+                audio_file_name,
+                "-vn",
+                "-ac",
+                "1",
+                "-c:a",
+                "libopus",
+                "-b:a",
+                "12k",
+                converted_file_name,
+            ],
+            check=True,
+            capture_output=True,
+            text=True,
+        )
+        if convert_process.returncode == 0:
+            os.remove(audio_file_name)
+    except subprocess.CalledProcessError as e:
+        st.error("Check uploaded file.", icon="🚨")
+        st.write(e.stderr)
+        st.stop()
+
+
+def summarize(converted_file_name=converted_file_name):
+    prompt = "Listen carefully to the following audio file. Provide a detailed summary."
+    audio_file = genai.upload_file(converted_file_name)
+    response = model.generate_content([prompt, audio_file])
+    genai.delete_file(audio_file.name)
+    return response.text
+
+
+def transcribe(model_name=st.session_state.model_name):
+    match model_name:
+        case "Balanced":
+            with open(converted_file_name, "rb") as audio:
+                transcription = replicate_client.run(
+                    "thomasmol/whisper-diarization:3ff22700b10e9c888e72235131e10c0a8427cd79e750bc42e4c035be2121796b",
+                    input={"file": audio, "transcript_output_format": "segments_only"},
+                )
+                return transcription
+        case _:
+            st.error("Model not found")
+            st.stop()
+
+
+def translate(
+    text, target_language=st.session_state.language, chunks=False, sleep_time=30
+):
+    prompt = f"Translate input text to {target_language}. Return only translated text: <input_text>{text}</input_text>"
+    try:
+        if chunks:
+            translation = model.generate_content(prompt)
+            time.sleep(
+                sleep_time
+            )  # 2 queries per minute https://ai.google.dev/gemini-api/docs/models/gemini#model-variations
+            return translation.text
+        else:
+            translation = model.generate_content(prompt)
+            return translation.text
+    except ValueError:
+        st.error(
+            "The translator thinks the content is unsafe and can't return the translation 🙈",
+            icon="🚨",
+        )
+        st.stop()
+
+
+def identify_speakers(transcription):
+    prompt = (
+        'Identify speakers names and replace "speaker" with identified name. For exmple <example_json>{"avg_logprob": -0.1729651133334914, "end": "238.19", "speaker": "SPEAKER_00", "start": "15.34", "text": "About six years ago."}</example_json>, return only json as example <example_return>{"SPEAKER_00":"Dave"}</example_return>. If you didnt identify names return the same name as was provided <example_return_without_identification>{"SPEAKER_00":"SPEAKER_00"}</example_return_without_identification>'
+        + f"Do it with this json <transcribed_json>{transcription}</transcribed_json>"
+    )
+    names = model.generate_content(prompt)
+    names_json = json.loads(names.text.split("```json")[1].split("```")[0])
+    return names_json
+
+
+def convert_to_minutes(seconds):
+    minutes, seconds = divmod(float(seconds), 60)
+    return f"{int(minutes)}:{int(seconds):02d}"
+
+
+def clean_up():
+    if os.path.isfile(converted_file_name):
+        os.remove(converted_file_name)
+
+
+def get_printable_results():
+    with st.spinner("Uploading the file to the server..."):
+        download()
+    with st.spinner("Compressing file..."):
+        compress_audio()
+    st.audio(converted_file_name)
+    if summary:
+        with st.spinner("Summarizing..."):
+            summary_results = summarize()
+            if target_language != None:
+                translation_results = translate(summary_results)
+                st.markdown(translation_results)
+            else:
+                st.markdown(summary_results)
+    else:
+        with st.spinner("Transcribing..."):
+            transcription = transcribe()
+            names = identify_speakers(transcription)
+            if target_language != None:
+                for segment in transcription["segments"]:
+                    st.markdown(
+                        f"**{convert_to_minutes(segment['start'])} - {str(segment['speaker']).replace(segment['speaker'], names[segment['speaker']])}:** {translate(segment['text'], chunks=True, sleep_time=30)}"
+                    )
+            else:
+                for segment in transcription["segments"]:
+                    st.markdown(
+                        f"**{convert_to_minutes(segment['start'])} - {str(segment['speaker']).replace(segment['speaker'], names[segment['speaker']])}:** {segment['text']}"
+                    )
+    clean_up()
+
+
+# Frontend
+st.title("Transcribe & Translate Audio Files")
+
+st.radio(
+    label="Choose what to transcribe:",
+    options=["Uploaded file", "YouTube link", "Audio file link"],
+    key="mode",
+)
+
+if st.session_state.mode == "Uploaded file":
+    uploaded_file = st.file_uploader(
+        "Choose a file:",
+        type=["wav", "mp3", "aiff", "aac", "ogg", "flac", "mpga", "m4a"],
+    )
+elif st.session_state.mode == "YouTube link":
+    yt_url = st.text_input(
+        label="Enter YouTube URL:",
+        placeholder="https://www.youtube.com/watch?v=z7-fPFtgRE4",
+    )
+elif st.session_state.mode == "Audio file link":
+    audio_link = st.text_input(
+        label="Enter the link to the file:",
+        placeholder="https://traffic.megaphone.fm/GLD4878952581.mp3",
+    )
+
+
+target_language = st.selectbox(
+    label="If you choose to translate, content will be returned translated.",
+    options=["English", "Ukrainian", "Spanish", "French", "German", "Russian"],
+    index=None,
+    placeholder="Select the target language if you need translation, or skip it if you don't",
+    label_visibility="collapsed",
+    key="language",
+)
+
+summary = st.checkbox("Generate summary (without transcription)")
+
+go = st.button("Go")
+
+# Data processing
+if go:
+    if st.session_state.mode == "Uploaded file":
+        if uploaded_file is not None:
+            get_printable_results()
+        else:
+            st.error("Upload an audio file.", icon="🚨")
+
+    elif st.session_state.mode == "YouTube link":
+        if len(yt_url.strip()) != 0:
+            get_printable_results()
+        else:
+            st.error("Enter a YouTube link.", icon="🚨")
+
+    elif st.session_state.mode == "Audio file link":
+        if len(audio_link.strip()) != 0:
+            get_printable_results()
+        else:
+            st.error("Enter an audio file link.", icon="🚨")
