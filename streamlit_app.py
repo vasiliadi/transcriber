06e4b83b9ce300db8c8209d9c109a8f93861b56d
vasiliadi <9432426+vasiliadi@users.noreply.github.com>
2024-09-25 00:10:59 -0500

    fix summarize

--- streamlit_app.py
+++ streamlit_app.py
@@ -157,14 +157,6 @@ def compress_audio(
         st.stop()
 
 
-@retry.Retry(predicate=retry.if_transient_error)
-def summarize(audio_file_name=AUDIO_FILE_NAME, prompt=st.session_state.summary_prompt):
-    audio_file = genai.upload_file(audio_file_name)
-    response = pro_model.generate_content([prompt, audio_file])
-    genai.delete_file(audio_file.name)
-    return response.text.replace("$", "\$")
-
-
 @st.cache_data(show_spinner=False)
 @retry.Retry(predicate=retry.if_transient_error)
 def summarize_with_transcription(transcription):
@@ -173,6 +165,29 @@ def summarize_with_transcription(transcription):
     )
     response = pro_model.generate_content(prompt)
     return response.text.replace("$", "\$")
+
+
+@retry.Retry(predicate=retry.if_transient_error)
+def summarize(
+    audio_file_name=AUDIO_FILE_NAME,
+    prompt=st.session_state.summary_prompt,
+    transcription_for_summary=st.session_state.transcription_for_summary,
+):
+    try:
+        audio_file = genai.upload_file(audio_file_name)
+        response = pro_model.generate_content([prompt, audio_file])
+        genai.delete_file(audio_file.name)
+        return response.text.replace("$", "\$")
+    except (exceptions.RetryError, TimeoutError, exceptions.DeadlineExceeded):
+        if transcription_for_summary:
+            compress_audio()
+            transcription = process_incredibly_fast_whisper(
+                diarization=False,
+                variant=INCREDIBLY_FAST_WHISPER,
+                post_processing=False,
+            )
+            transcription = transcription["segments"]
+            return summarize_with_transcription(transcription)
 
 
 @st.cache_data(show_spinner=False)
@@ -425,19 +440,7 @@ def process_summary():
     if not st.session_state.tts:
         st.audio(AUDIO_FILE_NAME)
     with st.spinner("Summarizing..."):
-        try:
-            summary_results = summarize()
-        except (exceptions.RetryError, TimeoutError, exceptions.DeadlineExceeded):
-            if st.session_state.transcription_for_summary:
-                compress_audio()
-                transcription = process_incredibly_fast_whisper(
-                    diarization=False,
-                    variant=INCREDIBLY_FAST_WHISPER,
-                    post_processing=False,
-                )
-                transcription = transcription["segments"]
-                summary_results = summarize_with_transcription(transcription)
-
+        summary_results = summarize()
         summary_results = translate(summary_results)
         st.markdown(summary_results)
     if st.session_state.tts:
