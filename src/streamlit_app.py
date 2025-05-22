c80dfefa4af6268b1078fef205f368f6b0344028
vasiliadi <9432426+vasiliadi@users.noreply.github.com>
2025-05-22 07:51:52 -0500

    🔧 chore(streamlit_app.py): update User-Agent string to latest Chrome version for accurate request simulation (#250)

--- src/streamlit_app.py
+++ src/streamlit_app.py
@@ -61,7 +61,7 @@ WHISPERX = "victor-upmeet/whisperx"
 # Headers for requests
 # https://www.whatismybrowser.com/guides/the-latest-user-agent/chrome
 headers = {
-    "User-Agent": "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/134.0.0.0 Safari/537.36",
+    "User-Agent": "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/136.0.0.0 Safari/537.36",
 }
 
 # Initialization
